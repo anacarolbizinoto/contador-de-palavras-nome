8623a33d71387d4fd412d3ffcb19764ac23f9fa9
Ana Carolina Bizinoto <acbsnutri@gmail.com>
2022-06-17 10:46:04 -0300

    incluindo linha no JS e algumas alterações de fonte no css

---
+++ script.js
@@ -0,0 +1,4 @@
+var nome = window.prompt("Qual é o seu nome?")
+document.writeln(`Seu nome tem ${nome.length} letras. <br/>`)
+document.writeln(`Seu nome em maiúsculas fica ${nome.toUpperCase()} <br/>`)
+document.writeln(`Seu nome em minúsculas fica ${nome.toLowerCase()}<br/>`)
